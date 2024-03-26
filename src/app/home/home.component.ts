923950f8c58107475e1cc43eff579d2081543d9f
jopdel <opdelochtjohn@gmail.com>
2024-03-26 10:23:35 +0100

    HOME - Login button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private router: Router) { }
+
   username: string | null | undefined;
-isLoggedIn: boolean = false;
+  isLoggedIn: boolean = false;
   ngOnInit() {
-    if (localStorage.getItem('username')){
+    if (localStorage.getItem('username')) {
       this.username = localStorage.getItem('username');
     }
-    if (localStorage.getItem('token')){
+    if (localStorage.getItem('token')) {
       this.isLoggedIn = true
     }
   }
+
+
+  goToLogin(): void {
+    this.router.navigateByUrl('/login');
+  }
+
 }
