58e7c8cf6c30a7d348ac3f172e6283723af58784
jopdel <opdelochtjohn@gmail.com>
2024-04-07 11:17:37 +0200

    GAME - Same functionality as admin, except for alert and task input field!

--- src/app/gametableplayer/gametableplayer.component.ts
+++ src/app/gametableplayer/gametableplayer.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import Pusher from 'pusher-js';
+import { GamesService } from '../shared/games.service';
 
 @Component({
   selector: 'app-gametableplayer',
@@ -16,12 +17,15 @@ import Pusher from 'pusher-js';
 })
 export class GametableplayerComponent {
 
-  constructor(private toastr: ToastrService, private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private gamesService: GamesService) { }
 
   gameCode: string | null | undefined;
   role: string | null | undefined;
-  selectedSet: number = 0;
-  gameName: string = "";
 
   username: string | null | undefined;
   scores: any[] = [];
@@ -30,12 +34,178 @@ export class GametableplayerComponent {
   tasks: any[] = [];
   task: string = '';
 
-  ngOnInit() {
+  myCards: any;
+  selectedCard: any; // Keep track of the selected card
+
+  setOfCards: any[] = [];
+
+  // Cardvalues for testgame
+  myCard: number = 0;
+  cardBot1: number = 0;
+  cardBot2: number = 0;
+  objectBot1: any;
+  objectBot2: any;
+
+  setOfCardName: string = "";
+  setOfCardID: number = 0;
+
+  regularCards = [
+    {
+      id: 1,
+      value: 1,
+      state: false
+    },
+    {
+      id: 2,
+      value: 2,
+      state: false
+    },
+    {
+      id: 3,
+      value: 3,
+      selected: false
+    },
+    {
+      id: 4,
+      value: 4,
+      selected: false
+    },
+    {
+      id: 5,
+      value: 5,
+      selected: false
+    },
+    {
+      id: 6,
+      value: 6,
+      selected: false
+    },
+    {
+      id: 7,
+      value: 7,
+      selected: false
+    },
+    {
+      id: 8,
+      value: 8,
+      selected: false
+    },
+    {
+      id: 9,
+      value: 9,
+      selected: false
+    },
+    {
+      id: 10,
+      value: 10,
+      selected: false
+    },
+    {
+      id: 11,
+      value: 11,
+      selected: false
+    },
+    {
+      id: 12,
+      value: 12,
+      selected: false
+    }
+  ]
+
+  fibonacciCards = [
+    {
+      id: 1,
+      value: 1,
+      state: false
+    },
+    {
+      id: 2,
+      value: 2,
+      state: false
+    },
+    {
+      id: 3,
+      value: 3,
+      state: false
+    },
+    {
+      id: 4,
+      value: 5,
+      state: false
+    },
+    {
+      id: 5,
+      value: 8,
+      state: false
+    },
+    {
+      id: 6,
+      value: 13,
+      state: false
+    },
+    {
+      id: 7,
+      value: 21,
+      state: false
+    },
+    {
+      id: 8,
+      value: 34,
+      state: false
+    },
+    {
+      id: 9,
+      value: 55,
+      state: false
+    },
+    {
+      id: 10,
+      value: 89,
+      state: false
+    },
+    {
+      id: 11,
+      value: 144,
+      state: false
+    },
+    {
+      id: 12,
+      value: 233,
+      state: false
+    }
+  ]
+
+  examples: Array<any> = [
+    { userId: '1', score: null },
+    { userId: '2', score: null },
+    { userId: '3', score: null },
+    { userId: '4', score: null },
+    { userId: '5', score: null },
+    { userId: '6', score: null },
+    { userId: '7', score: null },
+    { userId: '8', score: null }
+  ];
+
+  trackById(item: any): number {
+    return item.id;
+  }
+
+  async ngOnInit() {
     this.gameCode = localStorage.getItem('gamecode');
     this.role = localStorage.getItem('role');
+    this.username = localStorage.getItem('username');
 
-    if (localStorage.getItem('username')) {
-      this.username = localStorage.getItem('username');
+    // Retrieving details from our freshly created game by gamecode
+    const gameByGameCode = await this.gamesService.getGameByGamecode(this.gameCode);
+    // Fill in variable values
+    this.setOfCardName = gameByGameCode.setofcardname;
+    this.setOfCardID = gameByGameCode.setofcard_id;
+
+    // Changing values in setOfCards depending on setOfCardID
+    if (this.setOfCardID == 1) {
+      this.setOfCards = this.regularCards;
+    } else if (this.setOfCardID == 2) {
+      this.setOfCards = this.fibonacciCards;
     }
 
     // Enable pusher logging - don't include this in production
@@ -48,17 +218,29 @@ export class GametableplayerComponent {
     let channel = pusher.subscribe(this.gameCode!);
     channel.bind('score', (data: any) => {
       this.scores.push(data);
-      console.log(this.scores);
     });
 
     channel.bind('task', (data: any) => {
       this.tasks.push(data);
     });
+
+    for (let card of this.regularCards && this.fibonacciCards) {
+      card.state = false;
+    }
   }
 
+  // Changes the selected card state to true, changes all other cards state to false and stores value in myCard
+  onCardClick(card: any): void {
+    this.setOfCards.forEach((c: any) => {
+      c.state = false;
+    });
+    card.state = true;
+    this.myCard = card.value;
+    console.log("My card:", this.myCard);
+  }
 
   toLobby() {
-    const confirmed = window.confirm('Are you sure you want to leave this game?');
+    const confirmed = window.confirm('Are you sure you want to leave this game and return to the lobby?');
     if (confirmed) {
       localStorage.removeItem('role');
       localStorage.removeItem('gamecode');
@@ -84,34 +266,37 @@ export class GametableplayerComponent {
     }).subscribe(() => this.task = '');
   }
 
-  // gamesURL = this.gamesService.gamesURL;
-  // gamesArray: any[] = [];
-
-
-
-  // // Logic to display username in the header
-  // username: string | null | undefined;
-  // isLoggedIn: boolean = false;
-  // ngOnInit() {
-  //   if (localStorage.getItem('username')) {
-  //     this.username = localStorage.getItem('username');
-  //   }
-  //   if (localStorage.getItem('token')) {
-  //     this.isLoggedIn = true
-  //   }
-  //   this.initializeGames();
-  // }
-
-  // // bedoeling om te checken of gameCode bestaat --> joinGame
-  // async fetchGames() {
-  //   this.gamesArray = await this.gamesService.getGames();
-  // }
-
-  // // bedoeling om te checken of gameCode bestaat --> joinGame
-  // async initializeGames() {
-  //   await this.fetchGames();
-  //   const matchingGameCode = this.gamesArray.find(game => game.gamecode === '152456');
-  //   console.log(matchingGameCode);
-  // }
-
 }
+
+
+// gamesURL = this.gamesService.gamesURL;
+// gamesArray: any[] = [];
+
+
+
+// // Logic to display username in the header
+// username: string | null | undefined;
+// isLoggedIn: boolean = false;
+// ngOnInit() {
+//   if (localStorage.getItem('username')) {
+//     this.username = localStorage.getItem('username');
+//   }
+//   if (localStorage.getItem('token')) {
+//     this.isLoggedIn = true
+//   }
+//   this.initializeGames();
+// }
+
+// // bedoeling om te checken of gameCode bestaat --> joinGame
+// async fetchGames() {
+//   this.gamesArray = await this.gamesService.getGames();
+// }
+
+// // bedoeling om te checken of gameCode bestaat --> joinGame
+// async initializeGames() {
+//   await this.fetchGames();
+//   const matchingGameCode = this.gamesArray.find(game => game.gamecode === '152456');
+//   console.log(matchingGameCode);
+// }
+
+
